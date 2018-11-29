16c146f14b8106ace7eadea41d3a3553f3941133
Vader55 <38086201+barb03@users.noreply.github.com>
2018-11-29 10:18:03 -0600

    deleted unused files and made database

--- bamazon.sql
+++ bamazon.sql
@@ -12,3 +12,17 @@ price INTEGER(10),
 stock_quantity INTEGER(30),
 PRIMARY KEY (item_id)  
 );
+
+USE bamazon_db;
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("Adjustable Shocks", "Racecar", 57.25, 16);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("Stock Wheels", "Racecar", 125.55, 8);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("Engine", "Racecar", 3000, 5);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("Transaxle", "Racecar", 1525.36, 6);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("Helmet", "Driver", 325.88, 25);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("GLoves", "Driver", 36.47, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("Driver Suit", "Driver", 325.89, 10);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("Race Shoes", "Driver", 85.19, 20);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("Mech Rags", "Maintenance", 5.21, 355);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("Engine Oil", "Maintenance", 12.84, 200);
+
+SELECT * FROM products;
--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -1,5 +1,7 @@
 var mysql = require("mysql");
 require('dotenv').config();
+var chalk = require("chalk");
+const inquirer = require("inquirer");
 
 
 var connection = mysql.createConnection({
@@ -13,15 +15,72 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-
-  connection.query("SELECT * FROM products", function(err, res){
-    if(err) throw err;
-    console.log(res);
-    for(var i = 0; i < res.length; i++){
-      console.log(res[i].flavor);
-    }
-    connection.end();
-  })
-
+  makeTable();
 });
+
+var makeTable = function(){
+  connection.query("SELECT * FROM products", function(err, res){
+    for(var i = 0; i < res.length; i++){
+      console.log(res[i].item_id + " || " + res[i].product_name + " || " + res[i].department_name + " || " + 
+        res[i].price + " || " + res[i].stock_quantity + "\n");
+    }
+    promptCustomer(res);
+  })
+}
+var promptCustomer = function(res){
+  inquirer.prompt([
+  {
+    type: "input",
+    name: 'choice',
+    message: "What would you like to purchase? Q to quit"
+  }
+]).then(function(answer){
+      var correct = false;
+      if(answer.choice.toUpperCase() == "Q"){
+        process.exit();
+      }
+      for(var i = 0; i < res.length; i++){
+        if(res[i].product_name==answer.choice){
+          correct = true;
+          var product = answer.choice;
+          var id = i;
+          inquirer.prompt(
+            {
+            type: "input",
+            name: 'quant',
+            message: "How many would you like to buy?",
+            validate: function(value){
+              if(isNaN(value)==false){
+                return true;
+              }
+              else{
+                return false;
+              }
+            }
+          }).then (function(answer){           
+            if((res[id].stock_quantity-answer.quant) > 0){
+              connection.query("UPDATE products SET stock_quantity='" + (res[id].stock_quantity -
+                answer.quant) + "' WHERE product_name = '" + product + "'", function(err, res2){
+                  console.log("Product Bought");
+                  makeTable();
+                })                
+            }
+            else {
+              console.log("That is not a valid selection");
+              promptCustomer(res);
+            }
+          })
+        }
+      }
+      if(i==res.length && correct == false){
+        console.log("That is not a valid selection!");
+        promptCustomer(res);
+      }
+  })
+}
+
+
+
+
+
 
